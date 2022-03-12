298520995971177db0f5cdf6df33534ff450274d
Krishnavamsi <vamsik532@gmail.com>
2022-03-12 20:57:02 +0530

    updated urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('get_availability', view=views.view_tickets, name="availability"),
+    path('book_ticket', view=views.book_tickets, name="book"),
+    path('add_coach', view=views.add_coach, name="add coach"),
+    path('remove_coach', view=views.remove_coach, name="remove coach"),
+    path('view_all_seats', view=views.view_all_seats, name="view all seats"),
+    path('update_coach/<str:coach_type>',
+         view=views.update_coach_details, name="update coach")
 ]
